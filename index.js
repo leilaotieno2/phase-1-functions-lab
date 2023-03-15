7092ca59db711c57d6df016ea95a118b1f8bdb7b
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-03-15 23:00:16 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  const headquarters = 42;
+  return Math.abs(pickupLocation - headquarters);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  const distanceInBlocks = distanceFromHqInBlocks(pickupLocation);
+  const feetInBlock = 264;
+  return distanceInBlocks * feetInBlock;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const distanceInBlocks = Math.abs(endBlock - startBlock);
+  const feetInBlock = 264;
+  return distanceInBlocks * feetInBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance > 2500) {
+    return 'cannot travel that far';
+  } else if (distance > 2000) {
+    return 25;
+  } else if (distance > 400) {
+    return 0.02 * (distance - 400);
+  } else {
+    return 0;
+  }
+}
+
+const pickupLocation = 50;
+const startBlock = 34;
+const endBlock = 38;
+
+console.log(distanceFromHqInBlocks(pickupLocation));
+console.log(distanceFromHqInFeet(pickupLocation));
+console.log(distanceTravelledInFeet(startBlock, endBlock));
+console.log(calculatesFarePrice(startBlock, endBlock));
+
+
